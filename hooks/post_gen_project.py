492d7f20312c02efddd9fb63a9718f59c09cdd93
Joseph Maclean Arhin <josephmacleanarhin@outlook.com>
2021-12-28 02:37:38 +0000

    add pre_gen hooks to validate input

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -6,6 +6,7 @@ REMOVE_PATHS = [
     '{% if cookiecutter.project_database == "mongodb" %} migrations {% endif %}',
     '{% if cookiecutter.packaging != "pip" %} requirements.txt {% endif %}',
     '{% if cookiecutter.packaging != "poetry" %} poetry.lock {% endif %}',
+    '{% if cookiecutter.packaging != "poetry" %} pyproject.toml {% endif %}',
 ]
 
 for path in REMOVE_PATHS:
